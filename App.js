466d8efec353c22a929886c07cfafeefd7251d39
Yash Balla <ballayash@gmail.com>
2023-04-14 01:40:42 +0530

    test-1

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("p",{id : "yo"},"hello world fdgggggggggggggggg gffffffffffffffff sfggggggggggggggggggggggggg")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
